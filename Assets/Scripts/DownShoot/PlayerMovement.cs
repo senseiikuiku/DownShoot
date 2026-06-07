ea66297b5082ee6551b7d655f9b04be6d49cf204
senseiikuiku <dduc1171@gmail.com>
2026-06-07 09:46:58 +0700

    Setup new Input System

---
+++ Assets/Scripts/DownShoot/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerControls controls;
+
+    public Vector2 moveInput;
+    public Vector2 aimInput;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Character.Movement.performed += context => moveInput = context.ReadValue<Vector2>();
+        controls.Character.Movement.canceled += context => moveInput = Vector2.zero;
+
+        controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
+        controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
