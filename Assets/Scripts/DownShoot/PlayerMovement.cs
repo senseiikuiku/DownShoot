6dbf16195c06888373837653cc0050b2e21f786c
senseiikuiku <dduc1171@gmail.com>
2026-06-08 09:31:49 +0700

    Add: animations
    feat: setup animations

--- Assets/Scripts/DownShoot/PlayerMovement.cs
+++ Assets/Scripts/DownShoot/PlayerMovement.cs
@@ -1,21 +1,135 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls controls;
 
-    public Vector2 moveInput;
-    public Vector2 aimInput;
+    private CharacterController characterController;
+    private Animator animator;
+
+    [Header("Movement Info")]
+    [SerializeField] private float walkSpeed = 1.5f;
+    [SerializeField] private float runSpeed = 3f;
+    private float speed;
+    private Vector3 movementDirection;
+    private float verticalVelocity;
+    private bool isRunning;
+
+    [Header("Aim Info")]
+    [SerializeField] private Transform aim;
+    [SerializeField] private LayerMask aimLayerMask;
+    private Vector3 lookingDirection;
+
+
+    private Vector2 moveInput;
+    private Vector2 aimInput;
 
     private void Awake()
     {
+        AssignInputEvents();
+    }
+
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        speed = walkSpeed;
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+        ApplyAim();
+        AnimationControllers();
+    }
+
+    private void Shoot()
+    {
+        animator.SetTrigger("Fire");
+    }
+
+    private void AnimationControllers()
+    {
+        float xVelocity = Vector3.Dot(movementDirection.normalized, transform.right);
+        float zVelocity = Vector3.Dot(movementDirection.normalized, transform.forward);
+
+        animator.SetFloat("xVelocity", xVelocity, .1f, Time.deltaTime);
+        animator.SetFloat("zVelocity", zVelocity, .1f, Time.deltaTime);
+
+        bool playRunAnimation = isRunning && movementDirection.magnitude > 0;
+        animator.SetBool("isRunning", playRunAnimation);
+    }
+
+    private void ApplyAim()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
+        {
+            lookingDirection = hitInfo.point - transform.position;
+            lookingDirection.y = 0;
+            lookingDirection.Normalize();
+
+            transform.forward = lookingDirection;
+
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
+
+        ApplyGravity();
+
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * speed * Time.deltaTime);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (!characterController.isGrounded)
+        {
+            verticalVelocity -= 9.81f * Time.deltaTime;
+            movementDirection.y = verticalVelocity;
+        }
+        else
+        {
+            verticalVelocity = 0;
+        }
+    }
+
+    #region New Input System
+    private void AssignInputEvents()
+    {
         controls = new PlayerControls();
+
+        controls.Character.Fire.performed += contexts => Shoot();
 
         controls.Character.Movement.performed += context => moveInput = context.ReadValue<Vector2>();
         controls.Character.Movement.canceled += context => moveInput = Vector2.zero;
 
         controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
         controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
+
+        controls.Character.Run.performed += context =>
+        {
+            if (movementDirection.magnitude > 0)
+            {
+                speed = runSpeed;
+                isRunning = true;
+            }
+        };
+        controls.Character.Run.canceled += context =>
+        {
+            speed = walkSpeed;
+            isRunning = false;
+        };
     }
 
     private void OnEnable()
@@ -27,4 +141,5 @@ public class PlayerMovement : MonoBehaviour
     {
         controls.Disable();
     }
+    #endregion
 }
